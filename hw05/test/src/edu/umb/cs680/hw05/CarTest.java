9b41c83452dfb36e0eccce2604e6e6eaab907b93
VipulRana <vipulmrana0075@gmail.com>
2020-03-30 12:38:13 -0400

    Add files via upload

---
+++ hw05/test/src/edu/umb/cs680/hw05/CarTest.java
@@ -0,0 +1,21 @@
+package edu.umb.cs680.hw05;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CarTest {
+	
+	private String[] carToStringArray(Car car) {
+		String[] strarr = {car.getmake(), car.getmodel(), Integer.toString(car.getyear())};// as obj cannot be compared
+		return strarr;
+	}
+	
+	@Test
+	void verifyCarEqualityWithMakeModelYear() {
+		String[] expected = {"Toyota", "RAV4", "2018"};
+		Car actual = new Car("Toyota", "RAV4", 2018, 3, 3);
+		assertArrayEquals(expected, carToStringArray(actual));
+		}
+}
+
