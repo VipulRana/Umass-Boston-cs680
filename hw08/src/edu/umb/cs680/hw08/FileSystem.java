b621162c06284d2498fd5f15e80a84e1a14e8259
VipulRana <vipulmrana0075@gmail.com>
2020-04-14 19:08:50 -0400

    Add files via upload

---
+++ hw08/src/edu/umb/cs680/hw08/FileSystem.java
@@ -0,0 +1,29 @@
+package edu.umb.cs680.hw08;
+
+import java.util.LinkedList;
+
+public class FileSystem {
+
+	private static FileSystem instance = null;
+	private LinkedList<Directory> rootDirs;
+	public static FileSystem getFileSystem() {
+		if(instance==null)
+			instance = new FileSystem ();
+		return instance;
+	}
+	private FileSystem() {};
+	
+	public LinkedList<Directory> getRootDirs() {
+		return this.rootDirs;
+	}
+	
+	
+	public void addRootDir(Directory rootDir) {
+		rootDirs = new LinkedList<Directory>();
+		rootDirs.add(rootDir);
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("The class filesystem has run completely");
+	}
+}
